be714f755be4dfe848887138c1871bcc6bdce088
BadWaka <1456683844@qq.com>
2017-10-17 14:42:23 +0800

    bak

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,22 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    list: [
+      {
+        imgSrc: 'http://img3.imgtn.bdimg.com/it/u=2938123505,618018376&fm=27&gp=0.jpg',
+        title: 'X盒子',
+        text: '刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘'
+      },
+      {
+        imgSrc: 'http://img3.imgtn.bdimg.com/it/u=2938123505,618018376&fm=27&gp=0.jpg',
+        title: 'X盒子',
+        text: '刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘'
+      },
+      {
+        imgSrc: 'http://img3.imgtn.bdimg.com/it/u=2938123505,618018376&fm=27&gp=0.jpg',
+        title: 'X盒子',
+        text: '刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘德华（Andy Lau）刘'
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    ]
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
